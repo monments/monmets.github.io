3afa651483c6c297de5a2c69e565b904e2720872
monments <35414047+monments@users.noreply.github.com>
2018-01-14 14:17:54 +0800

    Create index.cc

---
+++ css/index.cc
@@ -0,0 +1,104 @@
+.header-section.has-img  .no-img {
+  margin-top： 0 ;
+  背景： #FCFCFC ;
+  保证金： 0  0  40 px ;
+  填充： 20 像素  0 ;
+  box-shadow： 0  0  5 px  #AAA ;
+}
+@media  只有 屏幕 和（最大宽度：365 像素）{
+  .header-section.has-img  .big-img {
+    margin-bottom： 0 ;
+  }
+}
+
+.list-filters {
+  字体家庭： '打开三世'， ' Helvetica Neue字体'，黑体， Arial字体，无衬线 ;
+  font-size： 18 px ;
+  margin-bottom： 30 px ;
+  text-align： center ;
+}
+
+.list-filters  .list-filter   {
+  文字装饰：无 ！重要 ;
+  保证金右： 15 像素 ;
+  边框： 1 px  solid  ＃777 ;
+  填充： 5 px  10 px ;
+  光标：指针 ;
+  显示：内联块 ;
+}
+
+.list-filters  .list-filter.filter-selected，
+.list-filters  .list-filter ：hover {
+  颜色： #fff ;
+  背景：＃444 ;
+}
+
+@media  只有 屏幕 和（最大宽度：550 像素）{
+  .list-filters  .list-filter {
+    font-size： 16 像素 ;
+    margin-right： 0 ;
+    margin-bottom： -1 px ;
+    显示：块 ;
+  }
+}
+
+.post-preview {
+  填充： 15 像素  10 像素 ;
+  显示：块 ;
+}
+
+@media  只有 屏幕 和（最小宽度：768 像素）{
+  .post-preview {
+    填充： 30 像素  10 像素 ;
+  }
+}
+
+.posts-list  文章 {
+  border-bottom： 1 px  solid  #eee ;
+}
+.posts-list  文章：last-child {
+  border-bottom： 0 ;
+}
+.posts-list  文章：悬停 {
+  背景：＃f5f5f5 ;
+}
+
+.post-preview {
+  文字装饰：无 ！重要 ;
+  颜色：＃404040  ！重要 ;
+}
+
+.post-preview  .post-title {
+  font-size： 32 px ;
+  font-weight： 700 ;
+  margin-top： 0 ;
+}
+@media  只有 屏幕 和（最大宽度：550 像素）{
+  .post-preview  .post-title {
+    font-size： 26 px ;
+  }
+}
+.post-preview  .post-subtitle {
+  保证金： 0 ;
+  font-weight： 300 ;
+  margin-bottom： 10 px ;
+  font-size： 20 px ;
+}
+
+.post-preview  .post-meta，
+.post-heading  .post-meta {
+  颜色：＃808080 ;
+  font-size： 18 px ;
+  font-style： italic ;
+  保证金： 0  0  10 px ;
+}
+.post-preview  .post-entry {
+  字体家庭： '打开三世'， ' Helvetica Neue字体'，黑体， Arial字体，无衬线 ;
+}
+.post-preview  .post-read-more {
+  font-weight： 800 ;
+}
+
+.post-preview  .post-entry {
+  显示：无 ;
+}
